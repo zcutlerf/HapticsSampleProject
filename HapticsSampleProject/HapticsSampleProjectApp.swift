0eaf60f456b5943e09ac81e5646e4e50a5c18cd5
Zoe Cutler <36316793+zcutlerf@users.noreply.github.com>
2023-05-24 09:34:12 -0400

    Initial Commit

---
+++ HapticsSampleProject/HapticsSampleProjectApp.swift
@@ -0,0 +1,17 @@
+//
+//  HapticsSampleProjectApp.swift
+//  HapticsSampleProject
+//
+//  Created by Zoe Cutler on 5/24/23.
+//
+
+import SwiftUI
+
+@main
+struct HapticsSampleProjectApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
